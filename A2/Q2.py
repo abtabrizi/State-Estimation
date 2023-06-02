4ef68fa3c89438546580fb6d5f2bb820c29a00ab
Arash <134088680+abtabrizi@users.noreply.github.com>
2023-06-02 14:56:58 -0400

    Draft 2 script for Q2.

--- A2/Q2.py
+++ A2/Q2.py
@@ -8,9 +8,9 @@ import imageio.v2 as imageio
 import os
 
 # User Settings
-Noise = True
-Linear = False
-Ellipse = True
+Noise = True 
+Linear = False # set to 'False' when simulating part (b)
+Ellipse = True 
 if Linear == True:
     part = "a"
 else:
@@ -25,7 +25,7 @@ screen = pygame.display.set_mode((scale, scale))
 sc_f = int(scale/20)
 cov_size = 20
 
-# Draw landmark
+# Draw landmark at (10, 10)
 Landmark = pygame.Surface((10, 10))
 Landmark.fill((0, 0, 0))
 L = array([[10, 10]]).T
@@ -58,20 +58,18 @@ thetaprev = 0
 X = array([[x0, y0]]).T
 P = zeros(2)
 
-# System parameters
+# Motion model setup
 r = 0.1
 rL = 0.2
 T = float(1/8)
 F = eye(2) #F remains identity matrix here
-
-#Variables
 ur = 4.75
 ul = 5.25
 u_w = (ur+ul)/2.0
 u_phi = (ur-ul)*(r/rL)
 U = array([[r*u_w*math.cos(theta),r*u_w*math.sin(theta)]]).T
 
-#Process Noise
+# Process Noise (motion model)
 ww = 0.1
 wphi = 0.01
 Q = diag([ww+wphi, ww+wphi])
@@ -80,7 +78,7 @@ delf_delphi = (T**2)*r*u_w* array([-math.sin(theta), math.cos(theta)])
 w_k = array([ww*delf_delw[0]+wphi*delf_delphi[0], ww*delf_delw[1]+wphi*delf_delphi[1]])
 Q_p = diag([w_k[0], w_k[1]])
 
-#Measurement parameters
+# Measurement setup 
 C = diag([1, 2])
 rx, ry = 0.05, 0.075
 R = diag([rx, ry])
@@ -90,11 +88,12 @@ dist_norm = 4.0
 # Other initializations
 i = 0
 running = True
+frames = [] # to store image frames
 
-
-#True line
+# True line
 true_line = [(x0*sc_f, y0*sc_f), (x0, y0)]
 
+# KF predictor definition
 def kf_predict(X, P, F, Q_p, U):
     noise_x = np.random.normal(0, ww)
     noise_y = np.random.normal(0, ww)
@@ -104,7 +103,7 @@ def kf_predict(X, P, F, Q_p, U):
     P = dot(F, dot(P, F.T)) + Q_p
     return X, P
 
-#Basic correction
+# KF corrector definition for part (a)
 def kf_correct(X, P, Y, C, R):
     IM = dot(C, X)
     IS = R + dot(C, dot(P, C.T))
@@ -113,8 +112,8 @@ def kf_correct(X, P, Y, C, R):
     P = P - dot(K, dot(C, P))
     return (X, P, K, IM, IS)
 
-#Range/bearing correction
-def kf_correct_bm(X, P, Y, G, R_p, IM):
+# KF corrector definition for part (b)
+def kf_correct_part_b(X, P, Y, G, R_p, IM):
     IS = R_p + dot(G, dot(P, G.T))
     det = IS[0,0]*IS[1,1] - IS[0,1]*IS[1,0]
     #try to clip outliers
@@ -133,10 +132,12 @@ def kf_correct_bm(X, P, Y, G, R_p, IM):
     X = X + dot(K, (Y - IM))
     P = P - dot(K, dot(G, P))
     return (X, P, K, IM, IS)
-    
+
+
 def range_bearing(d_norm, phi):
     return array([10+(d_norm*math.cos(phi)), 10+(d_norm*math.sin(phi))])
 
+# No noise simulation 
 if Noise == False:
     ww, wphi, rx, ry = 0, 0, 0, 0
 
@@ -144,6 +145,11 @@ if Noise == False:
 while running:
     i += 1 
     
+    # Capture the screen as image frames
+    frame = pygame.surfarray.array3d(screen)
+    frames.append(frame)
+    
+    # End simulation if simulation window closed
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
@@ -198,7 +204,7 @@ while running:
             G2 = array([(X[0, 0] - 10.0)*math.sin(phi), (X[1,0] - 10.0)*math.sin(phi)])
             G = (1.0/dist_norm)*array([G1, G2])
             #G = G.reshape(2, 2)
-            X, P, K, IM, IS = kf_correct_bm(X, P, Z, G, R_p, IM)
+            X, P, K, IM, IS = kf_correct_part_b(X, P, Z, G, R_p, IM)
             true_line[1] = (int(X[0, 0]*sc_f), int(X[1, 0]*sc_f))
             pygame.draw.lines(screen, (0, 0, 255), False, true_line, 3)
             true_line[0] = true_line[1]
@@ -241,6 +247,16 @@ while running:
 
 
     pygame.display.update()
+
+# Create animation GIF using imageio package 
+frames = [np.flip(frame, axis=1) for frame in frames]
+frames = [np.rot90(frame, k=1) for frame in frames]
+imageio.mimsave('animation-2b.gif', frames, duration=125)
+
+# Delete the PNG frames
+for file_name in os.listdir('.'):
+    if file_name.endswith('.png'):
+        os.remove(file_name)
 
 # Quit Pygame
 pygame.quit()
